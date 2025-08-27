719c0de4e22fc8bbff485ec8975568d66bfbadf0
Kevin <kevin@cube.nl>
2025-08-27 18:08:17 +0200

    Removed base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import handlebars from 'vite-plugin-handlebars'
 
 export default defineConfig({
-  base: '/am/',
   build: {
     rollupOptions: {
       input: {
